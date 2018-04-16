7443a2937259cb79e61a83de3d7ae4e42cee5096
gerard-pmh <darkrabbitlolagyar@gmail.com>
2018-04-17 01:12:19 +0200

    clean css solution

--- src/index.js
+++ src/index.js
@@ -1,31 +1,21 @@
-import { getDimensions, resizeSlides, resizeSlidesContainer } from './resize-utils'
-
-const ratio = 16 / 9
-
-const slidesContainer = document.querySelector('.slides-container')
 const slides = document.querySelector('.slides')
-
 const slideCount = slides.childElementCount
-
 let currentPosition = 0
 
-resize()
-window.addEventListener('resize', resize)
-
-document.addEventListener('keydown', function (e) {
+document.addEventListener('keydown', event => {
   slides.classList.add('enable-transition')
-  const code = e.keyCode ? e.keyCode : e.which
+
+  const code = event.keyCode ? event.keyCode : event.which
   if (code === 37 && currentPosition > 0) {
     currentPosition--
   } else if (code === 39 && currentPosition < slideCount - 1) {
     currentPosition++
   }
-  resizeSlides(slides, currentPosition, slideCount, { width: slidesContainer.offsetWidth })
+
+  slides.style.marginLeft = window.matchMedia('(max-aspect-ratio: 16/9)').matches
+    ? `-${currentPosition * 100}vw`
+    : `-${currentPosition * (16 / 9) * 100}vh`
+
 })
 
-function resize () {
-  slides.classList.remove('enable-transition')
-  const dimensions = getDimensions(ratio, window.innerWidth, window.innerHeight)
-  resizeSlidesContainer(slidesContainer, dimensions)
-  resizeSlides(slides, currentPosition, slideCount, dimensions)
-}
+window.onresize = () => slides.classList.remove('enable-transition')
